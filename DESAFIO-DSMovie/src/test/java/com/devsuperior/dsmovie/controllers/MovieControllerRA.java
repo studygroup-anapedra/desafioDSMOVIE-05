f3b5a91f6eea82e3ee0e25b86b8a5a6ccb2c34a5
anapedra <anapedra.mil@gmail.com>
2024-10-11 13:41:06 -0300

    Implementa teste de API para listagem de filmes sem parâmetros

--- DESAFIO-DSMovie/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ DESAFIO-DSMovie/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -2,11 +2,26 @@ package com.devsuperior.dsmovie.controllers;
 
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@SpringBootTest
+@AutoConfigureMockMvc
 public class MovieControllerRA {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() throws Exception {
+		mockMvc.perform(get("/movies"))
+				.andExpect(status().isOk())
+				.andDo(print());
 	}
 
 	@Test
